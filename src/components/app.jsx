e2a5b1137f7f923140e566fe5af8dd82029d4d12
Fabien Davy <davyfabien@gmail.com>
2020-03-26 19:25:05 +0100

    tried to implement the automatic scroll

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import MessageList from '../containers/message_list';
-import MessageForm from '../containers/message_form';
 
 const App = () => {
   return (
     <div className="app">
       <MessageList />
-      <MessageForm />
     </div>
   );
 };
--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import Message from '../components/message';
+import MessageForm from '../containers/message_form';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { fetchMessages } from '../actions';
 
 class MessageList extends Component {
+  // scrollToBottom() {
+  //   this.el.focus();
+  // }
 
   componentWillMount() {
     this.props.fetchMessages(this.props.selectedChannel);
+  }
+  
+  componentDidUpdate() {
+    this.list.scrollTop = this.list.scrollHeight;
   }
   
   componentDidMount() {
@@ -24,11 +32,12 @@ class MessageList extends Component {
         <div className="channel-title">
           <span>Channel #{this.props.selectedChannel}</span>
         </div>
-        <div className="channel-content">
+        <div className="channel-content" ref={(list) => { this.list = list; }}>
           {this.props.allMessages.map((message) => {
               return <Message message={message} key={message.created_at} />
             })}
         </div>
+        <MessageForm />
       </div>
     );
   }
